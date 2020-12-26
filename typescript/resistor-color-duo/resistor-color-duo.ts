c19c8d8035c0a6910d8ce73533a67e0a222df12c
krivtsov <krivtsov.su@gmail.com>
2020-12-27 01:49:25 +0400

    resolve ts resistor color duo

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,32 @@
 export class ResistorColor {
-  private colors: string[]
+  private colors: string[];
 
   constructor(colors: string[]) {
-    this.colors = colors
+    if (colors.length < 2) {
+      throw 'At least two colors need to be present';
+    }
+    this.colors = colors;
   }
-  value = (): number => 0
+
+  value = (): number | string => {
+    const colors = this.colors;
+
+    const colorsDict: { [key: string]: number } = {
+      black: 0,
+      brown: 1,
+      red: 2,
+      orange: 3,
+      yellow: 4,
+      green: 5,
+      blue: 6,
+      violet: 7,
+      grey: 8,
+      white: 9,
+    };
+
+    const firstColor: string = colors[0];
+    const secondColor: string = colors[1];
+
+    return colorsDict[firstColor] * 10 + colorsDict[secondColor];
+  };
 }
