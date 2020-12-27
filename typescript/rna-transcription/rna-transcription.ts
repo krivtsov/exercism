0b7b3966ff1d98be44afdb7b1c35a7c3fa17ff14
krivtsov <krivtsov.su@gmail.com>
2020-12-27 22:46:19 +0400

    resolve ts dna

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,7 +1,22 @@
 class Transcriptor {
-  toRna(/* Parameters go here */) {
-    // Your code here
+  toRna(dna: string): string {
+    const dnaDict: Record<string, string> = {
+      G: 'C',
+      C: 'G',
+      T: 'A',
+      A: 'U',
+    };
+    const dnaArr = dna.split('');
+    const rnaArr = dnaArr.map((elem) => {
+      const dnaToRna = dnaDict[elem];
+      if (!dnaToRna) {
+        throw 'Invalid input DNA.';
+      }
+      return dnaToRna;
+    });
+    const rna = rnaArr.join('');
+    return rna;
   }
 }
 
-export default Transcriptor
+export default Transcriptor;
