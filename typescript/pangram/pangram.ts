7807522c6b5de551cb1c05ee8892bfa4f4a6f7d2
krivtsov <krivtsov.su@gmail.com>
2022-05-27 00:50:24 +0400

    ts update resolve pangram

--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -1,3 +1,13 @@
-export function isPangram() {
-    throw new Error('Remove this statement and implement this function')
+export function isPangram(str: string): boolean {
+    const set = new Set();
+    const lettersReg = /^[A-Za-z]+$/;
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i].toLowerCase();
+        if(lettersReg.test(char)) {
+            set.add(char);
+        }
+    }
+    const countLetter =  set.size;
+    const alphabetSize = 26;
+    return countLetter === alphabetSize;
 }
