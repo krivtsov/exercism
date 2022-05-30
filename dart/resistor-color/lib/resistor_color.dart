1c3de440695b13ab3012e710e2ecea3ea0af8960
krivtsov <krivtsov.su@gmail.com>
2022-05-30 22:32:45 +0400

    dart resolve resictor_color

--- dart/resistor-color/lib/resistor_color.dart
+++ dart/resistor-color/lib/resistor_color.dart
@@ -1,3 +1,23 @@
 class ResistorColor {
-  // Put your code here
+  ResistorColor() {}
+
+  static const COLORS = <String>[
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+  ];
+
+  List<String> get colors => COLORS;
+
+  int colorCode(String s) {
+    final index = COLORS.indexOf(s);
+    return index;
+  }
 }
