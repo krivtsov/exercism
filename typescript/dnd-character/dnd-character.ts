a46154305943572a3a10d14814131ad2d07539a3
krivtsov <krivtsov.su@gmail.com>
2022-05-30 22:41:53 +0400

    ts resolve dnd-character

--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -1,9 +1,33 @@
 export class DnDCharacter {
-  public static generateAbilityScore(): number {
-    throw new Error('Remove this statement and implement this function')
-  }
+    hitpoints: number;
+    constitution: number;
+    strength: number;
+    dexterity: number;
+    intelligence: number;
+    wisdom: number;
+    charisma: number;
 
-  public static getModifierFor(abilityValue: number): number {
-    throw new Error('Remove this statement and implement this function')
-  }
+    constructor() {
+        this.constitution = DnDCharacter.generateAbilityScore();
+        this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
+        this.strength = DnDCharacter.generateAbilityScore();
+        this.dexterity = DnDCharacter.generateAbilityScore();
+        this.intelligence = DnDCharacter.generateAbilityScore();
+        this.wisdom = DnDCharacter.generateAbilityScore();
+        this.charisma = DnDCharacter.generateAbilityScore();
+    }
+
+    public static generateAbilityScore(): number {
+        const getRandom = (): number => (Math.floor(Math.random() * 6) + 1);
+        const num1 = getRandom();
+        const num2 = getRandom();
+        const num3 = getRandom();
+        const num4 = getRandom();
+        const minNum = Math.min(num1, num2, num3, num4);
+        return num1 + num2 + num3 + num4 - minNum;
+    }
+
+    public static getModifierFor(abilityValue: number): number {
+        return Math.floor((abilityValue - 10) / 2);
+    }
 }
