ca55454ff9462399d0cc9798eb56218152901fc4
krivtsov <krivtsov.su@gmail.com>
2022-05-31 00:54:16 +0400

    dart fix resolve leap

--- dart/leap/lib/leap.dart
+++ dart/leap/lib/leap.dart
@@ -1,11 +1,13 @@
 class Leap {
-  bool leapYear(int year) {
-    if (year % 4 == 0) {
-      if (year % 100 == 0 && year % 400 != 0) {
-        return false;
-      }
-      return true;
-    }
-    return false;
-  }
+  // bool leapYear(int year) {
+  //   if (year % 4 == 0) {
+  //     if (year % 100 == 0 && year % 400 != 0) {
+  //       return false;
+  //     }
+  //     return true;
+  //   }
+  //   return false;
+  // }
+
+  bool leapYear(int year) => (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
 }
