7a1ebb55d2c79cc02422de69aa3c891c5bf43eb2
Krivtsov <krivtsov.su@gmail.com>
2023-02-21 23:27:16 +0400

    resolve elixir freelanceRates

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,21 @@
 defmodule FreelancerRates do
+  @spec daily_rate(number) :: float
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    hourly_rate * 8 * 1.0
   end
 
+  @spec apply_discount(number, number) :: float
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount * (100 - discount) / 100
   end
 
+  @spec monthly_rate(number, number) :: integer
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    trunc(Float.ceil(apply_discount(daily_rate(hourly_rate) * 22, discount)))
   end
 
+  @spec days_in_budget(number, number, number) :: float
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    Float.floor(budget / apply_discount(daily_rate(hourly_rate), discount), 1)
   end
 end
