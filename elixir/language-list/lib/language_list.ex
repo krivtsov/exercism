2a79f4181de4d7c0173cf275b0d2191e21cdafb6
Krivtsov <krivtsov.su@gmail.com>
2023-02-23 23:43:00 +0400

    resolve elixir language-list

--- elixir/language-list/lib/language_list.ex
+++ elixir/language-list/lib/language_list.ex
@@ -1,25 +1,31 @@
 defmodule LanguageList do
+  @spec new :: []
   def new() do
-    # Please implement the new/0 function
+    []
   end
 
+  @spec add(nonempty_maybe_improper_list, any) :: nonempty_maybe_improper_list
   def add(list, language) do
-    # Please implement the add/2 function
+    [language | list]
   end
 
-  def remove(list) do
-    # Please implement the remove/1 function
+  @spec remove(nonempty_maybe_improper_list) :: any
+  def remove([_ | t]) do
+    t
   end
 
-  def first(list) do
-    # Please implement the first/1 function
+  @spec first(nonempty_maybe_improper_list) :: any
+  def first([h | _]) do
+    h
   end
 
+  @spec count(list) :: non_neg_integer
   def count(list) do
-    # Please implement the count/1 function
+    length list
   end
 
+  @spec functional_list?(any) :: boolean
   def functional_list?(list) do
-    # Please implement the functional_list?/1 function
+    "Elixir" in list
   end
 end
