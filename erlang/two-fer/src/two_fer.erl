fb5ae340cd70043f022b8461872135003b4d763d
Krivtsov <krivtsov.su@gmail.com>
2023-02-24 19:53:24 +0400

    resolve erlang two-fer

--- erlang/two-fer/src/two_fer.erl
+++ erlang/two-fer/src/two_fer.erl
@@ -2,7 +2,6 @@
 
 -export([two_fer/0, two_fer/1]).
 
+two_fer() -> two_fer("you").
 
-two_fer() -> undefined.
-
-two_fer(_Name) -> undefined.
+two_fer(Name) -> "One for " ++ Name ++ ", one for me.".
