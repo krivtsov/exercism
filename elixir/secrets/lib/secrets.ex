2b90d4f43c52919a5b1f607ebc2ea9157de6447d
Krivtsov <krivtsov.su@gmail.com>
2023-02-24 14:15:05 +0400

    resolve elixir secrets

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,29 +1,37 @@
 defmodule Secrets do
+  @spec secret_add(number) :: (number -> number)
   def secret_add(secret) do
-    # Please implement the secret_add/1 function
+    fn param -> param + secret end
+    # &(&1 + secret)
   end
 
+  @spec secret_subtract(number) :: (number -> number)
   def secret_subtract(secret) do
-    # Please implement the secret_subtract/1 function
+    fn param -> param - secret end
   end
 
+  @spec secret_multiply(number) :: (number -> number)
   def secret_multiply(secret) do
-    # Please implement the secret_multiply/1 function
+    fn param -> param * secret end
   end
 
+  @spec secret_divide(integer) :: (integer -> integer)
   def secret_divide(secret) do
-    # Please implement the secret_divide/1 function
+    fn param -> param |> div(secret) end
   end
 
+  @spec secret_and(integer) :: (integer -> integer)
   def secret_and(secret) do
-    # Please implement the secret_and/1 function
+    fn param -> param |> Bitwise.band(secret) end
   end
 
+  @spec secret_xor(integer) :: (integer -> integer)
   def secret_xor(secret) do
-    # Please implement the secret_xor/1 function
+    fn param -> param |> Bitwise.bxor(secret) end
   end
 
+  @spec secret_combine(any, any) :: (any -> any)
   def secret_combine(secret_function1, secret_function2) do
-    # Please implement the secret_combine/2 function
+    &(&1 |> secret_function1.() |>secret_function2.())
   end
 end
