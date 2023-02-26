82007a5b4382921198048cc1551aa390640b39f4
Krivtsov <krivtsov.su@gmail.com>
2023-02-27 00:03:29 +0400

    resolve erlang accumulate

--- erlang/accumulate/src/accumulate.erl
+++ erlang/accumulate/src/accumulate.erl
@@ -5,4 +5,7 @@
 %% given a fun and a list, apply fun to each list item replacing list item with fun's return value.
 %%
 -spec accumulate(fun((A) -> B), list(A)) -> list(B).
-accumulate(_Fn, _List) -> undefined.
+accumulate(_, []) ->
+    [];
+accumulate(Fn, [H | T]) ->
+    [Fn(H) | accumulate(Fn, T)].
