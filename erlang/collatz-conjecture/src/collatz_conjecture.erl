34dbd7b50d91159264dbcaa6461cba4084cd7b59
Krivtsov <krivtsov.su@gmail.com>
2023-03-01 01:11:54 +0400

    resolve erlang collatz_conjecture

--- erlang/collatz-conjecture/src/collatz_conjecture.erl
+++ erlang/collatz-conjecture/src/collatz_conjecture.erl
@@ -2,5 +2,9 @@
 
 -export([steps/1]).
 
+steps(N) when N > 0-> steps(N, 0);
+steps(_) -> error(badarg).
 
-steps(_N) -> undefined.
+steps(1, Count) -> Count;
+steps(N, Count) when N rem 2 == 0 -> steps(N div 2, Count + 1);
+steps(N, Count) -> steps(N * 3 + 1, Count + 1).
