147a71ea6674f13cfdbe01831ab1ef17b4dfa7e3
Krivtsov <krivtsov.su@gmail.com>
2023-03-06 00:22:10 +0400

    resolve rust assembly-line

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,7 +1,16 @@
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {speed}")
+    let produced_each_hour = 221;
+    let success_rate: f64;
+    if speed <= 4 {
+        success_rate = 1.0
+    } else if speed <= 8 {
+        success_rate = 0.9
+    } else {
+        success_rate = 0.77
+    }
+    return f64::from(speed) * f64::from(produced_each_hour) * success_rate;
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {speed}")
+    (production_rate_per_hour(speed) / 60.0) as u32
 }
