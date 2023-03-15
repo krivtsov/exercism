939a48ac99def2587949d12c289a05f81cc0532e
Krivtsov <krivtsov.su@gmail.com>
2023-03-15 23:50:24 +0400

    resolve typescript bob

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,3 +1,27 @@
-export function hey(message: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function hey(message: string): string {
+  const messageTrim = message.trim();
+
+  const isUpperCase = messageTrim.toLocaleUpperCase() === messageTrim;
+  const isQuestion = messageTrim.endsWith('?');
+  const isLetter = message.match(/[a-zA-Z]/);
+  const isEmpty = messageTrim.length === 0;
+  const isScream = isLetter && isUpperCase;
+
+  if (isEmpty) {
+    return 'Fine. Be that way!';
+  }
+
+  if (isScream && isQuestion) {
+    return "Calm down, I know what I'm doing!";
+  }
+
+  if (isScream) {
+    return 'Whoa, chill out!';
+  }
+
+  if (isQuestion) {
+    return 'Sure.';
+  }
+
+  return 'Whatever.';
 }
