cc10722148ddcf869e8d5114a1589d1cf4bcc3d4
Krivtsov <krivtsov.su@gmail.com>
2023-03-22 00:40:13 +0400

    update typescript helloworld

--- typescript/hello-world/hello-world.ts
+++ typescript/hello-world/hello-world.ts
@@ -1,7 +1,3 @@
-class HelloWorld {
-  static hello(name: string = 'World'): string {
+export const hello = (name: string = 'World'): string => {
     return `Hello, ${name}!`;
-  }
 }
-
-export default HelloWorld;
