594ce7c0f80f765f840673bc2dc28fb7393f879e
Krivtsov <krivtsov.su@gmail.com>
2023-03-21 21:08:27 +0400

    resolve typerscript matrix

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,13 +1,31 @@
 export class Matrix {
-  constructor() {
-    throw new Error('Remove this statement and implement this function')
+  #rows: number[][];
+  #columns: number[][];
+
+  constructor(matrix: string) {
+    this.#rows = matrix.split('\n').map((val: string): number[] => {
+      const stringArr = val.split(' ');
+      return stringArr.map((elem) => parseInt(elem, 10));
+    });
+
+    const lengthMatrix = this.#rows.length;
+    const columns: number[][] = [];
+    for (let i = 0; i < lengthMatrix; i++) {
+      columns[i] = new Array(lengthMatrix);
+    }
+    for (let i = 0; i < lengthMatrix; i++) {
+      for (let j = 0; j < lengthMatrix; j++) {
+        columns[j][i] = this.#rows[i][j];
+      }
+    }
+    this.#columns = columns;
   }
 
-  get rows(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get rows(): number[][] {
+    return this.#rows;
   }
 
-  get columns(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get columns(): number[][] {
+    return this.#columns;
   }
 }
