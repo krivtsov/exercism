e3d313f9f0fa92aeb5cbd587ad0550837c9bbab4
Krivtsov <krivtsov.su@gmail.com>
2023-03-25 16:08:49 +0400

    resolve elixir log level

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,9 +1,27 @@
 defmodule LogLevel do
+  @spec to_label(Number, Boolean) ::
+          :debug | :error | :fatal | :info | :trace | :unknown | :warning
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    cond do
+      level == 0 && !legacy? -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 && !legacy? -> :fatal
+      true -> :unknown
+    end
   end
 
+  @spec alert_recipient(Number, Boolean) :: :dev1 | :dev2 | false | :ops
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    label = to_label(level, legacy?)
+
+    cond do
+      label in [:error, :fatal] -> :ops
+      label == :unknown && legacy? -> :dev1
+      label == :unknown -> :dev2
+      true -> false
+    end
   end
 end
