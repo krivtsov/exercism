ea63c70144a6f303d7b004c89319d4d52337f185
Krivtsov <krivtsov.su@gmail.com>
2023-03-27 22:45:46 +0400

    resolve elixir guessing game

--- elixir/guessing-game/lib/guessing_game.ex
+++ elixir/guessing-game/lib/guessing_game.ex
@@ -1,5 +1,23 @@
 defmodule GuessingGame do
+  @spec compare(Number, Number) :: <<_::56, _::_*8>>
   def compare(secret_number, guess) do
-    # Please implement the compare/2 function
+    case {secret_number, guess} do
+      {secret_number, guess} when is_number(guess) ->
+        cond do
+          secret_number == guess -> "Correct"
+          secret_number + 1 == guess -> "So close"
+          secret_number - 1 == guess -> "So close"
+          secret_number < guess -> "Too high"
+          secret_number > guess -> "Too low"
+          true -> "Make a guess"
+        end
+
+      {_, _} ->
+        "Make a guess"
+    end
+  end
+
+  def compare(_) do
+    "Make a guess"
   end
 end
