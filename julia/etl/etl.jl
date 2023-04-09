c943e2c389a41171b08ce48a5c88c4348411610c
Krivtsov <krivtsov.su@gmail.com>
2023-04-09 23:40:54 +0400

    resolve julia etl

--- julia/etl/etl.jl
+++ julia/etl/etl.jl
@@ -1,4 +1,10 @@
 function transform(input::AbstractDict)
-
+    result = Dict()
+    for (index, value) in input
+        for (_, val) in enumerate(value)
+            result[lowercase(val)] = index
+        end
+    end
+    return result;
 end
 
