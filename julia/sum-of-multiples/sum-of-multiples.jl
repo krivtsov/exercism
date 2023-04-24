6a1a2ac62ec70c01ebb5248b2771894d9425dd03
Krivtsov <krivtsov.su@gmail.com>
2023-04-24 23:46:42 +0400

    resolve julia sum multi

--- julia/sum-of-multiples/sum-of-multiples.jl
+++ julia/sum-of-multiples/sum-of-multiples.jl
@@ -1,2 +1,18 @@
 function sum_of_multiples(limit, factors)
+    resVec = []
+    for factor in factors
+        if factor == 0
+            continue
+        end
+        for cnt in 1:((limit -1) ÷ factor) 
+            push!(resVec, cnt * factor)
+        end
+    end
+    uniqueVec = Set(resVec)
+    result = 0
+    for elem in uniqueVec
+        result += elem
+    end
+    result
 end
+
