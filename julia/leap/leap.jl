f8eca345c57cf2f247566c4f37e2f691b6dddd9d
Krivtsov <krivtsov.su@gmail.com>
2023-04-28 22:43:27 +0400

    resolve julia leap

--- julia/leap/leap.jl
+++ julia/leap/leap.jl
@@ -5,6 +5,12 @@ Return `true` if `year` is a leap year in the gregorian calendar.
 
 """
 function is_leap_year(year)
-
+    if year % 400 == 0
+        return true
+    end
+    if year % 100 == 0
+        return false
+    end
+    year % 4 == 0
 end
 
